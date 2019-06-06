29e8531295133dacd7d9ccf9705cefe49f334dfd
Zhengyang Liu <liuzhengyang183@gmail.com>
2019-06-06 20:37:42 +0800

    make file browser

--- DataUploader/MainWindow.xaml.cs
+++ DataUploader/MainWindow.xaml.cs
@@ -24,10 +24,10 @@ namespace DataUploader
         {
             InitializeComponent();
             DatabaseManager databaseManager = new DatabaseManager();
-            string tableName = "SensorData";
-            databaseManager.CreateTable(tableName);
-            databaseManager.Upsert(tableName, DateTime.Now, 0.01f);
-            databaseManager.Upsert(tableName, DateTime.Now, 100f);
+            //string tableName = "SensorData";
+            //databaseManager.CreateTable(tableName);
+            //databaseManager.Upsert(tableName, DateTime.Now, 0.01f);
+            //databaseManager.Upsert(tableName, DateTime.Now, 100f);
         }
     }
 }
